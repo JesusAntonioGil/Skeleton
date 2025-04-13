9f24a7507550cb05217d22b2199617789ccfc36f
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-04-13 12:49:50 +0200

    Create project

---
+++ Skeleton/Skeleton/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Skeleton
+//
+//  Created by Jesus Antonio Gil on 13/4/25.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ Skeleton/Skeleton/SkeletonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkeletonApp.swift
+//  Skeleton
+//
+//  Created by Jesus Antonio Gil on 13/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SkeletonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
